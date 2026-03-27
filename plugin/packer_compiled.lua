3ca8a62ee48896e0771eb106aebda68850a9563e
Jesper Hemmingsson <jesper.hemmingsson@gmail.com>
2026-03-27 09:21:19 +0100

    Update packer_compiled.lua

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/jesperhemmingsson/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/jesperhemmingsson/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/jesperhemmingsson/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/jesperhemmingsson/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/jesperhemmingsson/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/Users/jesperhemmingsson/.cache/nvim/packer_hererocks/2.1.1748459687/share/lua/5.1/?.lua;/Users/jesperhemmingsson/.cache/nvim/packer_hererocks/2.1.1748459687/share/lua/5.1/?/init.lua;/Users/jesperhemmingsson/.cache/nvim/packer_hererocks/2.1.1748459687/lib/luarocks/rocks-5.1/?.lua;/Users/jesperhemmingsson/.cache/nvim/packer_hererocks/2.1.1748459687/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/jesperhemmingsson/.cache/nvim/packer_hererocks/2.1.1748459687/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -78,6 +78,12 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/jesperhemmingsson/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
+  },
+  ["claudecode.nvim"] = {
+    config = { "\27LJ\2\n8\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\15claudecode\frequire\0" },
+    loaded = true,
+    path = "/Users/jesperhemmingsson/.local/share/nvim/site/pack/packer/start/claudecode.nvim",
+    url = "https://github.com/coder/claudecode.nvim"
   },
   ["cmp-buffer"] = {
     loaded = true,
@@ -134,11 +140,6 @@ _G.packer_plugins = {
     path = "/Users/jesperhemmingsson/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
-  ["neo-todo.nvim"] = {
-    loaded = true,
-    path = "/Users/jesperhemmingsson/.local/share/nvim/site/pack/packer/start/neo-todo.nvim",
-    url = "/Users/jesperhemmingsson/code/neo-todo.nvim"
-  },
   ["nvim-cmp"] = {
     loaded = true,
     path = "/Users/jesperhemmingsson/.local/share/nvim/site/pack/packer/start/nvim-cmp",
@@ -184,8 +185,13 @@ _G.packer_plugins = {
     path = "/Users/jesperhemmingsson/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
+  ["snacks.nvim"] = {
+    loaded = true,
+    path = "/Users/jesperhemmingsson/.local/share/nvim/site/pack/packer/start/snacks.nvim",
+    url = "https://github.com/folke/snacks.nvim"
+  },
   ["ssr.nvim"] = {
-    config = { "\27LJ\2\nÜ\1\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\4\0=\3\5\2B\0\2\1K\0\1\0\fkeymaps\1\0\5\nclose\6q\16replace_all\17<leader><cr>\20replace_confirm\t<cr>\15prev_match\6N\15next_match\6n\1\0\5\14max_width\3x\vborder\frounded\15min_height\3\5\14min_width\0032\15max_height\3\25\nsetup\bssr\frequire\0" },
+    config = { "\27LJ\2\nå\1\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\4\0=\3\5\2B\0\2\1K\0\1\0\fkeymaps\1\0\5\15next_match\6n\16replace_all\17<leader><cr>\20replace_confirm\t<cr>\15prev_match\6N\nclose\6q\1\0\6\14max_width\3x\vborder\frounded\14min_width\0032\15min_height\3\5\fkeymaps\0\15max_height\3\25\nsetup\bssr\frequire\0" },
     loaded = false,
     needs_bufread = false,
     only_cond = false,
@@ -244,6 +250,10 @@ end
 time([[Config for trouble.nvim]], true)
 try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\ftrouble\frequire\0", "config", "trouble.nvim")
 time([[Config for trouble.nvim]], false)
+-- Config for: claudecode.nvim
+time([[Config for claudecode.nvim]], true)
+try_loadstring("\27LJ\2\n8\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\15claudecode\frequire\0", "config", "claudecode.nvim")
+time([[Config for claudecode.nvim]], false)
 -- Config for: todo-comments.nvim
 time([[Config for todo-comments.nvim]], true)
 try_loadstring("\27LJ\2\n?\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\18todo-comments\frequire\0", "config", "todo-comments.nvim")
